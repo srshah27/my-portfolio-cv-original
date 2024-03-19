d2ce662003e20270bb0e6ab249262fb929206d93
srshah27 <snehilshah27@gmail.com>
2024-03-19 12:59:35 +0530

    switch to mysql

--- src/lib/database.js
+++ src/lib/database.js
@@ -1,5 +1,19 @@
-import { PrismaClient } from "@prisma/client";
+import mysql from 'mysql2';
 
-const db = new PrismaClient();
+const pool = mysql.createPool({
+	host: 'roundhouse.proxy.rlwy.net',
+	user: 'root',
+	database: 'railway',
+	password: 'gijSriyVNEsQZyNBRmmQcAYcpufDhUaI',
+  port: 49810,
+	waitForConnections: true,
+	connectionLimit: 10,
+	maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+	idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+	queueLimit: 0,
+	enableKeepAlive: true,
+	keepAliveInitialDelay: 0
+});
 
-export default db;
+
+export default pool.promise();
--- src/routes/api/blogs/+server.js
+++ src/routes/api/blogs/+server.js
@@ -2,7 +2,7 @@ import db from '$lib/database';
 import { json } from '@sveltejs/kit';
 
 export async function GET() {
-	const posts = await db.post.findMany();
-
+	const [posts, columns] = await db.execute('SELECT * FROM `Post`');
+	console.log(columns)
 	return json(posts);
 }
